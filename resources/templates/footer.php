e0942398407b40704a09da6bc1e6bc4f0479c780
ojaula <lau_lundmark@hotmail.com>
2014-11-30 21:50:00 +0100

    updated footer
    eng and responsive

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,83 +1,74 @@
-<footer class="footer">
-    <section class="wrapper_footer"  >
-        <article class="column leftlist">
-            <h4>ME</h4>
-            <ul>
-                <li><a href="javascript:void(0)">Profile</a></li>
-                <li><a href="javascript:void(0)">Shopping Cart</a></li>
-                <li><a href="javascript:void(0)">Orders</a></li>
-                <li><a href="javascript:void(0)">Shop</a></li>
-            </ul>
-        </article>
-        <article class="column midlist">
-            <h4>WE</h4>
-            <ul>
-                <li><a href="javascript:void(0)">About us</a></li>
-                <li><a href="javascript:void(0)">Gallery</a></li>
-                <li><a href="javascript:void(0)">Help</a></li>
-                <li><a href="javascript:void(0)">Contact us</a></li>
-            </ul>
-        </article>
-        <article class="column rightlist">
-            <h4>Dolor sit amet</h4>
-            <ul>
-                <li><a href="javascript:void(0)"><img src="images/icons/ic1.jpg" width="50px" alt="some alt text"/><span>Unterwegs traf es eine Copy. Die Copy warnte.</span></a></li>
-                <li><a href="javascript:void(0)"><img src="images/icons/ic4.jpg" width="50px" alt="some alt text"/><span>Doch alles Gutzureden konnte es nicht.</span></a></li>
+<footer class="footer" >
+    <div class="wrapper_footer " style="float: none; max-width: 900px; margin: 0 auto;">
+        <section class="inner_wrapper_footer" style="padding-top: 20px">
+            <div class="row">
+                <div class=" col-xs-12 col-md-4">
+                    <div class="column_wrapper">
+                        <article class="column midlist" >
+                            <h4>YOU</h4>
+                            <ul>
+                                <li><a href="javascript:void(0)">Profile</a></li>
+                                <li><a href="javascript:void(0)">Shop</a></li>
+                                <li><a href="javascript:void(0)">Shopping cart</a></li>
+                                <li><a href="javascript:void(0)">Orders</a></li>
+                            </ul>
+                        </article>
+                    </div>
+                </div>
+                <div class="col col-xs-12 col-md-4" >
+                    <div class="column_wrapper">
+                        <article class="column midlist" >
+                            <h4>WE</h4>
+                            <ul>
+                                <li><a href="javascript:void(0)">About us</a></li>
+                                <li><a href="javascript:void(0)">Gallery</a></li>
+                                <li><a href="javascript:void(0)">Help</a></li>
+                                <li><a href="javascript:void(0)">Contact us</a></li>
+                            </ul>
+                        </article>
+                    </div>
+                </div>
 
-            </ul>
-        </article>
+                <div class="col-xs-12 col-md-4" >
+                    <div class="column_wrapper">
+                        <article class="column midlist" >
+                            <p>
+                                This homepage is an student project, where you can draw images online.
+                                <br>
+                                <br>
+                                The site is not fully functional and it is correctly under construction.
+                                I the near feature you will be able to registry and buy tools for enchanting the drawing experience.
+                                If you have any questions or ideas to improve this site send Paul an email.
 
-        <section id="copyright_wrapper" class="legalnotices_section_style">
-            <h3 class="hidden" style="display:inline-block;">Copyright notice</h3>
-            <!-- display as inline-block for horizontal placement of text-->
-            <p style="display:inline-block;">
-                Student Project 2014 by <a href="http://www.ltu.se">Johan Lundmark and Paul Dixon</a>. All Rights Reserved.
-            </P>
-            <div class="social" style="display:inline-block;">
-                <a href="javascript:void(0)"><img src="images/icons/s.png" alt="google plus" width="40"/></a>
-                <a href="javascript:void(0)"><img src="images/icons/tw.png" alt="tumblr" width="40"/></a>
-                <a href="javascript:void(0)"><img src="images/icons/f.png" alt="youtube" width="40"/></a>
-                <a href="javascript:void(0)"><img src="images/icons/r.png" alt="bebo" width="40"/></a>
-                <a href="javascript:void(0)"><img src="images/icons/sp.png" alt="xing" width="40"/></a>
+                            </p>
+                        </article>
+                    </div>
+                </div>
             </div>
         </section>
-    </section>
-</footer>
 
-<div class="row center-block " style="float: none; max-width: 900px;">
-    <div class="col-xs-12 col-md-6">
-        <h3 class="panel-title">Brushes</h3>
+        <div class="row" id=" copyright_wrapper" >
+            <h3 class="hidden">Copyright notice</h3>
 
+            <div class="col-xs-12 col-md-12" style="color: #bbbbbb;" >
+                <div class="column_wrapper">
+                    Student Project 2014 by<a href="http://www.ltu.se">Johan Lundmark and Paul Dixon</a>. All Rights Reserved.</P>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-12 " >
+                <div class="column_wrapper">
+                    <a href="javascript:void(0)"><img src="images/icons/s.png" alt="google plus" width="40"/></a>
+                    <a href="javascript:void(0)"><img src="images/icons/tw.png" alt="tumblr" width="40"/></a>
+                    <a href="javascript:void(0)"><img src="images/icons/f.png" alt="youtube" width="40"/></a>
+                    <a href="javascript:void(0)"><img src="images/icons/r.png" alt="bebo" width="40"/></a>
+                    <a href="javascript:void(0)"><img src="images/icons/sp.png" alt="xing" width="40"/></a>
+                </div>
+            </div>
+        </div>
 
-        <!-- COLOR -->
-        <div class="col-xs-2 col-md-2">
-            <article class="column midlist">
-                <h4>WE</h4>
-                <ul>
-                    <li class=" glyphicon glyphicon-pencil" ><a href="javascript:void(0)">About us</a></li>
-                    <li><a href="javascript:void(0)">Gallery</a></li>
-                    <li><a href="javascript:void(0)">Help</a></li>
-                    <li><a href="javascript:void(0)">Contact us</a></li>
-                </ul>
-            </article>
-        </div>
-        <div class="col-xs-2 col-md-2">
-            <article class="column midlist">
-                <h4>WE</h4>
-                <ul>
-                    <li><a href="javascript:void(0)">About us</a></li>
-                    <li><a href="javascript:void(0)">Gallery</a></li>
-                    <li><a href="javascript:void(0)">Help</a></li>
-                    <li><a href="javascript:void(0)">Contact us</a></li>
-                </ul>
-            </article>
-        </div>
-        <div class="col-xs-2 col-md-2">
-            <button type="button" class="btn btn-default btn-xs glyphicon glyphicon-pencil">8px
-            </button>
-        </div>
     </div>
-</div>
+
+</footer>
 
 <!--load bootstrap scripts -->
 <script src="js/bootstrap.min.js"></script>
